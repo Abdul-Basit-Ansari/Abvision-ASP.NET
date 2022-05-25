69a543c8f43bdcd4e2f833d840842b4c808901b3
Abdul Basit <abdulbasitansari288@gmail.com>
2022-05-26 00:13:06 +0500

    Add project files.

---
+++ AbVision/Controllers/AbController.cs
@@ -0,0 +1,27 @@
+﻿using AbVision.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AbVision.Controllers
+{
+    public class AbController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Course()
+        {
+            return View();
+        }
+         public IActionResult About()
+        {
+            return View();
+        }
+
+        
+        
+    }
+}
